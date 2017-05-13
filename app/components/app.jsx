4ee8cdee0f2b4370c9190da6c9ef33e07f348060
Michael Glitzos <mglitzos@outlook.com>
2017-05-12 20:38:26 -0400

    Initial Commit

---
+++ app/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Main application component
+ *
+ * @export
+ * @returns {React.Element} React application element
+ */
+export default function App() {
+  return <h1>Hello from React!</h1>;
+}
